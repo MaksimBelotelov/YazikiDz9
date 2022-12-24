782b83c19bc328ab709df682efd34e3e995f8bb0
Maksim <maksim_belotelov@mail.ru>
2022-12-24 21:22:41 +0300

    add Ex66

---
+++ Ex66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66. Задайте значения M и N. Напишите программу, которая найдет
+// сумму натуральных элементов в промежутке от M до N c помощью рекурсии
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int from = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите число N: ");
+int to = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Cумма натуральных чисел от {from} до {to} равна {GetSum(from, to)}");
+
+int GetSum(int from, int to)
+{
+    if(to>=from) return to+GetSum(from,to-1);
+    else return 0;
+}
